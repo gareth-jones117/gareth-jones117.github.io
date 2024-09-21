d22f4da1a74d847cc9f9d291f20641b98cbe1d9e
Gareth-Jones <garethjonescode@gmail.com>
2024-09-21 14:04:30 +1200

    intial commit

---
+++ JavaScript modules/main.js
@@ -0,0 +1,10 @@
+import { encode } from './encode.js'
+
+const input = document.getElementById('PlainTextArea')
+const output = document.getElementById('CodedText')
+
+input.addEventListener('change', (event) => {
+  const plaintext = input.value
+  const codedText = encode(plaintext)
+  output.textContent = codedText
+})
